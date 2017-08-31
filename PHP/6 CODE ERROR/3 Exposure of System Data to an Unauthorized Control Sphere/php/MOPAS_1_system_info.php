be0aca9b566ca784a7fd4244f28830e0a738de94
PTSECURITY\omachalov <omachalov@ptsecurity.com>
2017-08-31 12:36:24 +0700

    Delete

--- PHP/6 CODE ERROR/3 Exposure of System Data to an Unauthorized Control Sphere/php/MOPAS_1_system_info.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- *  MOPAS Exposure of System Data to an Unauthorized Control Sphere Example 1
- */
-phpinfo(INFO_MODULES);
-?>
--- PatternMatching/php/SensitiveCookie.php
+++
@@ -1,9 +0,0 @@
-<?php
-	function NotSecureCookies()
-	{
-		session_set_cookie_params(1800, '/', '.com');
-		$value = 'something from somewhere';
-		setcookie('TestCookie', $value);
-		setrawcookie('TestCookie2', $value);
-	}
-?>
