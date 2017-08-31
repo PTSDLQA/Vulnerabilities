c0137284a87c6a554a8e58453721a4a8a14e4829
PTSECURITY\omachalov <omachalov@ptsecurity.com>
2017-08-31 12:38:55 +0700

    Delete

--- PatternMatching/php/InsecureTransport.php
+++
@@ -1,9 +0,0 @@
-<?php
-    function InsecureTransportSample()
-    {
-        // The call uses the http:// protocol instead of https:// to send data to the server
-        $client = new Zend_Http_Client('http://www.example.com/fetchdata.php');
-        $client = new Zend_Http_Client('https://www.example.com/fetchdata.php');
-        //$client = new Zend_Http_Client('http://www.example.com/fetchdata.php');
-    }
-?>
