c77e406a5537c56185c2b66d85c442a6ddbc68d7
We are hire! <pt.sdl.qa@gmail.com>
2018-01-18 14:21:58 +0700

    Update Ldap.aspx.cs

--- CSharp/Tests/1 INPUT DATA VERIFICATION/9 LDAP Injection/Ldap.aspx.cs
+++ CSharp/Tests/1 INPUT DATA VERIFICATION/9 LDAP Injection/Ldap.aspx.cs
@@ -60,13 +60,13 @@ namespace Mopas.Tests
 
             // this is our fourth vulnerability of XSS in this file
             // we will demonstrate what happen if developer want to cheat (FIXED Marker)
-             // VEIFY: AI issue #687, Medium, XSS, http://omachalov.ptsecurity.ru/#/taskResults/1233
+             // VERIFY: AI issue #687, Medium, XSS, http://omachalov.ptsecurity.ru/#/taskResults/1233
              // GET /Tests/1%20INPUT%20DATA%20VERIFICATION/9%20LDAP%20Injection/Ldap.aspx.cs?name=%3cscript%3ealert(1)%3c%2fscript%3e HTTP/1.1
              // Host: localhost
              // FP: AI issue #683, Medium, XSS, http://omachalov.ptsecurity.ru/#/taskResults/1227
              // GET /Tests/1%20INPUT%20DATA%20VERIFICATION/9%20LDAP%20Injection/Ldap.aspx.cs?name=%3cscript%3ealert(1)%3c%2fscript%3e HTTP/1.1
              // Host: localhost
-             // TODO: AI issue #691, Medium, XSS, http://omachalov.ptsecurity.ru/#/taskResults/1239
+             // FIXED: AI issue #691, Medium, XSS, http://omachalov.ptsecurity.ru/#/taskResults/1239
              // GET /Tests/1%20INPUT%20DATA%20VERIFICATION/9%20LDAP%20Injection/Ldap.aspx.cs?name=%3cscript%3ealert(1)%3c%2fscript%3e HTTP/1.1
              // Host: localhost
              Response.Write(name);
