c4ea26cb3071f8329a07f151fc059af7da56fd20
We are hire! <pt.sdl.qa@gmail.com>
2018-01-18 13:37:38 +0700

    Update Ldap.aspx.cs

--- CSharp/Tests/1 INPUT DATA VERIFICATION/9 LDAP Injection/Ldap.aspx.cs
+++ CSharp/Tests/1 INPUT DATA VERIFICATION/9 LDAP Injection/Ldap.aspx.cs
@@ -39,28 +39,28 @@ namespace Mopas.Tests
 
             // this is our first vulnerability of XSS in this file
             // we will demonstrate False Positive scenario here (FP Marker)
-            // TODO: AI issue #661, Medium, XSS, http://omachalov.ptsecurity.ru/#/taskResults/1200
+            // FP: AI issue #661, Medium, XSS, http://omachalov.ptsecurity.ru/#/taskResults/1200
             // GET /Tests/1%20INPUT%20DATA%20VERIFICATION/9%20LDAP%20Injection/Ldap.aspx.cs?name=%3cscript%3ealert(1)%3c%2fscript%3e HTTP/1.1
             // Host: localhost
             Response.Write(name);
 
             // this is our second vulnerability of XSS in this file
             // we will demonstrate what happen if developer fails with his fix (VERIFY Marker)
-            // TODO: AI issue #662, Medium, XSS, http://omachalov.ptsecurity.ru/#/taskResults/1201
+            // FIXED: AI issue #662, Medium, XSS, http://omachalov.ptsecurity.ru/#/taskResults/1201
             // GET /Tests/1%20INPUT%20DATA%20VERIFICATION/9%20LDAP%20Injection/Ldap.aspx.cs?name=%3cscript%3ealert(1)%3c%2fscript%3e HTTP/1.1
             // Host: localhost
             Response.Write(name);
 
             // this is our third vulnerability of XSS in this file
             // we will demonstrate what happen if we really fix vulnerability (VERIFY Marker)
-             // TODO: AI issue #663, Medium, XSS, http://omachalov.ptsecurity.ru/#/taskResults/1202
+             // VERIFY: AI issue #663, Medium, XSS, http://omachalov.ptsecurity.ru/#/taskResults/1202
              // GET /Tests/1%20INPUT%20DATA%20VERIFICATION/9%20LDAP%20Injection/Ldap.aspx.cs?name=%3cscript%3ealert(1)%3c%2fscript%3e HTTP/1.1
              // Host: localhost
-             Response.Write(name);
+             Response.Write("name");
 
             // this is our fourth vulnerability of XSS in this file
             // we will demonstrate what happen if developer want to cheat (FIXED Marker)
-             // TODO: AI issue #664, Medium, XSS, http://omachalov.ptsecurity.ru/#/taskResults/1203
+             // VERIFY: AI issue #664, Medium, XSS, http://omachalov.ptsecurity.ru/#/taskResults/1203
              // GET /Tests/1%20INPUT%20DATA%20VERIFICATION/9%20LDAP%20Injection/Ldap.aspx.cs?name=%3cscript%3ealert(1)%3c%2fscript%3e HTTP/1.1
              // Host: localhost
              Response.Write(name);
